c96c77d82f588ab61f41da2f6e94aa77330ea7b0
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-10-24 09:17:54 +0530

    Add files via upload

---
+++ pushback.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s="Ayush";
+    cout<<s<<endl;
+    s.push_back('i');
+    s.push_back('k');
+    s.push_back('a');
+    cout<<s<<endl;
+}
