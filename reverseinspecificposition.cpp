14907989901cab399446afb690d365f8fab3cd0a
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-10-26 09:39:47 +0530

    Add files via upload

---
+++ reverseinspecificposition.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="Ayush Shivam";
+    cout<<s<<endl;
+    int n=s.length();
+    reverse(s.begin()+4,s.begin()+7);
+    cout<<s<<endl;
+}
